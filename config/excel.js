1f9e1bbc798ed64e3a9e07990920861cee4eb34f
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-10 02:01:32 +0700

    refactor: update schema

--- config/excel.js
+++ config/excel.js
@@ -3,14 +3,14 @@ module.exports = {
     "api::rsvp.rsvp": {
       columns: ["attendance_code", "is_attended", "attended_at"],
       relation: {
-        user_id: {
-          column: ["id"],
+        user: {
+          column: ["name"],
         },
-        event_id: {
-          column: ["id"],
+        event: {
+          column: ["title"],
         },
-        rsvp_status_id: {
-          column: ["id"],
+        rsvp_status: {
+          column: ["status"],
         },
       },
       locale: "false",
