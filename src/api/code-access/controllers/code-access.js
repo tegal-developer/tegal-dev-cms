6aa47f93f4b2eabd07ce493acd20abfcb3d8cd29
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-11 02:36:59 +0700

    Refactor: update schema

--- src/api/code-access/controllers/code-access.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * code-access controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::code-access.code-access');
--- src/api/code-access/routes/code-access.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * code-access router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::code-access.code-access');
