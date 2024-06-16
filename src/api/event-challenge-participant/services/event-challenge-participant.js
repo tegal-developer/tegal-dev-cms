17e00cb41777b7f62aec8949acd01c3eb4ca904c
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-16 13:51:35 +0700

    refactor: update

--- src/api/event-challenge-participant/services/event-challenge-participant.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-challenge-participant service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::event-challenge-participant.event-challenge-participant');
