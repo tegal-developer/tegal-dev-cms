607cf23df60aff3555a6fb4b0f3ad7240402a1fe
Musthafa Faishal <musthafafaishal@gmail.com>
2024-06-16 14:42:49 +0700

    refactor: update schema

--- src/api/event-challenge-participant-status/controllers/event-challenge-participant-status.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-challenge-participant-status controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::event-challenge-participant-status.event-challenge-participant-status');
--- src/api/event-challenge-participant/controllers/event-challenge-participant.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-challenge-participant controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::event-challenge-participant.event-challenge-participant');
